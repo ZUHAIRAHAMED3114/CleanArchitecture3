6103b083e1f9aa6492acb2a7479ab4cf0235ee13
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-10-27 10:19:49 +0530

    Intial Folder Stetup

---
+++ eShop.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,26 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PlugInInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class SearchProduct : ISearchProduct
+    {
+        private readonly IProductRepository _productRepository;
+
+        public SearchProduct(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IEnumerable<Product> Execute(string filter)
+        {
+
+            return _productRepository.GetProducts(filter);
+
+
+        }
+    }
+}
---
+++ eShop.UseCases/ViewProductScreen/ViewProduct.cs
@@ -0,0 +1,23 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PlugInInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class ViewProduct : IViewProduct
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ViewProduct(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Product Execute(int id)
+        {
+            return _productRepository.GetProduct(id);
+        }
+    }
+}
