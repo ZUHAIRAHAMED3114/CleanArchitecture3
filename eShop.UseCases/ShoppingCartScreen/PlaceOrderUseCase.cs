fc7ef645e3c5e2c9b3a30054607da86a3a687282
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-11-04 11:52:22 +0530

    Working on Place Order

---
+++ eShop.UseCases/ShoppingCartScreen/PlaceOrderUseCase.cs
@@ -0,0 +1,44 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.CoreBusiness.Services;
+using eShop.UseCases.PlugInInterfaces.DataStore;
+using eShop.UseCases.PlugInInterfaces.StateStore;
+using eShop.UseCases.PlugInInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.ShoppingCartScreen
+{
+    public class PlaceOrderUseCase : IPlaceOrderUseCase
+    {
+        private readonly OrderServices orderService;
+        private readonly IOrderRepository orderRepository;
+        private readonly IShoppingCart shoppingCart;
+        private readonly IShoppingCartStateStore shoppingCartStateStore;
+
+        public PlaceOrderUseCase(OrderServices orderService,
+                                 IOrderRepository orderRepository,
+                                 IShoppingCart shoppingCart,
+                                 IShoppingCartStateStore shoppingCartStateStore)
+        {
+            this.orderService = orderService;
+            this.orderRepository = orderRepository;
+            this.shoppingCart = shoppingCart;
+            this.shoppingCartStateStore = shoppingCartStateStore;
+        }
+        public async Task<string> Execute(Order order)
+        {
+            if (orderService.ValidateCreateOrder(order))
+            {
+                order.DatePlaced = DateTime.Now;
+                order.UniqueId = Guid.NewGuid().ToString();
+                orderRepository.CreateOrder(order);
+                await shoppingCart.EmptyAsync();
+                shoppingCartStateStore.UpdateLineItemsCount();
+                return order.UniqueId;
+            }
+            return null;
+        }
+    }
+}
